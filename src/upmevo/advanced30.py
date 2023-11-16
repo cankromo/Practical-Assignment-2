2cdc17184aa6d2c4c5367ae79999ecb4785a8f9b
thybris <thybrisvdb@outlook.com>
2023-11-16 13:50:23 +0100

    advanced genetic

---
+++ src/upmevo/advanced30.py
@@ -0,0 +1,141 @@
+import random
+
+# Define problem parameters
+tasks = 30
+resources = 28
+task_duration = [5, 3, 1, 1, 5, 2, 2, 8, 9, 1,
+                 4, 2, 4, 1, 1, 3, 7, 6, 1, 8,
+                 3, 3, 10, 8, 7, 4, 5, 2, 7, 5]
+task_resource = [8, 5, 1, 7, 1, 4, 1, 4, 5, 1,
+                 7, 11, 1, 6, 1, 1, 10, 9, 8, 1,
+                 1, 1, 1, 8, 8, 1, 1, 9, 1, 3]
+task_dependencies = [(1, 4), (2, 5), (2, 16), (2, 25), (3, 9), (3, 13),
+                     (3, 18), (4, 11), (4, 15), (4, 17), (5, 6), (5, 7),
+                     (5, 14), (6, 8), (6, 10), (6, 19), (7, 21), (8, 22),
+                     (9, 20), (10, 12), (11, 16), (11, 30), (12, 28), (13, 26),
+                     (14, 18), (14, 28), (15, 25), (15, 26), (16, 26), (16,27),
+                     (17, 18), (17, 24), (18, 27), (19, 24), (20, 29), (21, 23),
+                     (22, 30), (23, 27), (24, 30), (25, 28), (26, 29), (27, 29)]
+population_size = 50
+generations = 100
+mutation_rate = 0.2
+max_no_improvement = 10  # Termination condition: Stop if no improvement for this many generations
+
+# Generate a random seed from 0 to 10
+seed = random.randint(0, 10)
+random.seed(seed)
+
+# Initialize a population of schedules
+def initialize_population(population_size):
+    population = []
+    for _ in range(population_size):
+        schedule = random.sample(range(1, tasks + 1), tasks)
+        population.append(schedule)
+    return population
+
+# Calculate makespan for a schedule
+def calculate_makespan(schedule):
+    task_finish_time = [0] * tasks
+    for task in schedule:
+        dependencies = [dependency for dependency in task_dependencies if dependency[1] == task]
+        if dependencies:
+            start_time = max(task_finish_time[dependency[0] - 1] for dependency in dependencies)
+        else:
+            start_time = 0
+        end_time = start_time + task_duration[task - 1]
+        
+        # Check for resource availability and non-overlapping tasks
+        resource = task_resource[task - 1]
+        if all(task_finish_time[i] <= start_time or task_resource[i] != resource for i in range(task)):
+            task_finish_time[task - 1] = end_time
+        else:
+            # If there's an overlap, adjust the start time
+            start_time = max(task_finish_time[i] for i in range(task))
+            end_time = start_time + task_duration[task - 1]
+            task_finish_time[task - 1] = end_time
+    return max(task_finish_time)
+
+# Selection: Tournament selection
+def tournament_selection(population, k=5):
+    selected = random.sample(population, k)
+    return min(selected, key=calculate_makespan)
+
+# Crossover: Two-point crossover with non-overlapping constraint
+def crossover(parent1, parent2):
+    point1, point2 = random.sample(range(1, tasks), 2)
+    if point1 > point2:
+        point1, point2 = point2, point1
+    
+    # Ensure non-overlapping tasks
+    child1 = [task for task in parent1 if task not in parent2[point1:point2]]
+    child2 = [task for task in parent2 if task not in parent1[point1:point2]]
+    
+    return child1[:point1] + parent2[point1:point2] + child1[point1:], child2[:point1] + parent1[point1:point2] + child2[point1:]
+
+# Mutation: Swap mutation with non-overlapping constraint
+def mutate(schedule):
+    if random.random() < mutation_rate:
+        point1, point2 = random.sample(range(tasks), 2)
+        
+        # Ensure non-overlapping tasks
+        while schedule[point1] in schedule[point2:point2 + 2] or schedule[point2] in schedule[point1:point1 + 2]:
+            point1, point2 = random.sample(range(tasks), 2)
+        
+        schedule[point1], schedule[point2] = schedule[point2], schedule[point1]
+    return schedule
+
+# Define a function to select the best population
+def select_best_population(population, size):
+    return sorted(population, key=calculate_makespan)[:size]
+
+# Genetic Algorithm
+def advanced_genetic_algorithm():
+    global mutation_rate  # Declare mutation_rate as global
+    
+    population = initialize_population(population_size)
+    best_schedule = population[0]
+    best_makespan = calculate_makespan(best_schedule)
+    no_improvement_count = 0
+    elite_size = int(0.1 * population_size)  # Percentage of elite individuals
+
+    for generation in range(generations):
+        new_population = []
+        
+        # Apply genetic operators to create a new population
+        for _ in range(population_size - elite_size):
+            parent1 = tournament_selection(population)
+            parent2 = tournament_selection(population)
+            child1, child2 = crossover(parent1, parent2)
+            child1 = mutate(child1)
+            child2 = mutate(child2)
+            new_population.extend([child1, child2])
+        
+        # Keep the elite individuals from the previous population
+        elite = select_best_population(population, elite_size)
+        new_population.extend(elite)
+
+        # Select the best solutions for the next generation
+        population = select_best_population(new_population, population_size)
+        
+        # Check for improvement in best makespan
+        new_makespan = calculate_makespan(population[0])
+        if new_makespan < best_makespan:
+            best_schedule = population[0]
+            best_makespan = new_makespan
+            no_improvement_count = 0
+        else:
+            no_improvement_count += 1
+
+        # Decrease mutation rate over time
+        mutation_rate = max(0.05, mutation_rate * 0.95)
+
+        # Termination condition: Stop if no improvement for a certain number of generations
+        if no_improvement_count >= max_no_improvement:
+            break
+
+    return best_schedule, best_makespan
+
+best_schedule, makespan = advanced_genetic_algorithm()
+print("Best Schedule:", best_schedule)
+print("Makespan:", makespan)
+print("Random Seed:", seed)
